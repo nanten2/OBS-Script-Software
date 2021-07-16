45da152065598fd95bb08da5ff76731145cd1af0
Jeremyptq <jerem.p127@gmail.com>
2021-07-16 10:57:44 +0900

    _

--- scripts/Gvars.py
+++ scripts/Gvars.py
@@ -1,9 +1,11 @@
 import tkinter as tk
 
+
 def tlwind_init():
     """tl_windows keeps track of the number of top-level windows"""
     global tl_windows
     tl_windows = []
+
 
 def tkroot_init():
     """Create Tkinter root"""
